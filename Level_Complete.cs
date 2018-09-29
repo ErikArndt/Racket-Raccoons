b301f375dc400640237f49408ef59ba81c2a104a
taempura <43689509+taempura@users.noreply.github.com>
2018-09-29 15:26:24 -0400

    Add files via upload

---
+++ Level_Complete.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_Complete : MonoBehaviour {
+    public GameObject display;
+    private bool GoalReached = false;
+
+
+	// Use this for initialization
+	void Start () {
+        display.gameObject.SetActive(false);
+
+	}
+
+    void ToggleVisibility(){
+        if (GoalReached == true)
+        {
+            display.gameObject.SetActive(true);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0)) {
+            GoalReached = true;
+            ToggleVisibility();
+        }
+	}
+}
