68c3c0d26c15eb3a14262801edd5efc1c62ba5ac
HoodieSticks <39469696+HoodieSticks@users.noreply.github.com>
2018-09-29 11:15:14 -0400

    Add files via upload

---
+++ Attractor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour {
+
+    // Adapted from code by Brackeys
+    // https://www.youtube.com/watch?v=Ouu3D_VHx9o
+
+    const float G = 1;
+
+    public Rigidbody2D rb;
+
+    private void FixedUpdate()
+    {
+        Attractor[] attractors = FindObjectsOfType<Attractor>();
+        foreach (Attractor attractor in attractors)
+        {
+            if (attractor != this)
+            {
+                Attract(attractor);
+            }
+        }
+    }
+
+    void Attract(Attractor objectToAttract)
+    {
+        Rigidbody2D rbToAttract = objectToAttract.rb;
+
+        Vector2 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector2 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+    }
+}
