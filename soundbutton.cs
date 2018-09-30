021e654edfda2a6042869a0637210dfa7f88b0fe
taempura <43689509+taempura@users.noreply.github.com>
2018-09-30 15:01:47 -0400

    Add files via upload

---
+++ soundbutton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class soundbutton : MonoBehaviour {
+    AudioSource m_MyAudioSource;
+
+    void Start()
+    {
+        Button my_Button = GetComponent<Button>();
+        m_MyAudioSource = GetComponent<AudioSource>();
+        Debug.Log("declared");
+
+        my_Button.onClick.AddListener(PlayMusic);
+    }
+
+    void PlayMusic()
+    {
+        Debug.Log("playing");
+        m_MyAudioSource.Play();
+    }
+
+
+    void Update()
+    {
+
+
+    }
+
+}
+
+
